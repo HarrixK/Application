04c09d4e18b07ef70ab29f778f502f754981f450
Haris Ullah Khan <harrixkhan5515@gmail.com>
2020-02-04 13:40:34 +0500

    Initial commit

---
+++ app/src/main/java/com/example/harriscityapplication/Karachi.java
@@ -0,0 +1,14 @@
+package com.example.harriscityapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Karachi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_karachi_information);
+    }
+}
